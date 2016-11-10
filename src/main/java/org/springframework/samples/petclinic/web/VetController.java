9e6dc2c1c4dc2d188c267445133ec07df3c5bd01
Dave Syer <dsyer@pivotal.io>
2016-11-10 16:51:14 +0000

    Convert to jar with thymeleaf

--- src/main/java/org/springframework/samples/petclinic/web/VetController.java
+++ src/main/java/org/springframework/samples/petclinic/web/VetController.java
@@ -35,32 +35,28 @@ public class VetController {
 
     private final ClinicService clinicService;
 
-
     @Autowired
     public VetController(ClinicService clinicService) {
         this.clinicService = clinicService;
     }
 
-    @RequestMapping(value = { "/vets.html"})
+    @RequestMapping(value = { "/vets.html" })
     public String showVetList(Map<String, Object> model) {
-        // Here we are returning an object of type 'Vets' rather than a collection of Vet objects
-        // so it is simpler for Object-Xml mapping
+        // Here we are returning an object of type 'Vets' rather than a collection of Vet
+        // objects so it is simpler for Object-Xml mapping
         Vets vets = new Vets();
         vets.getVetList().addAll(this.clinicService.findVets());
         model.put("vets", vets);
         return "vets/vetList";
     }
 
-    @RequestMapping(value = { "/vets.json", "/vets.xml"})
-    public
-    @ResponseBody
-    Vets showResourcesVetList() {
-        // Here we are returning an object of type 'Vets' rather than a collection of Vet objects
-        // so it is simpler for JSon/Object mapping
+    @RequestMapping(value = { "/vets.json", "/vets.xml" })
+    public @ResponseBody Vets showResourcesVetList() {
+        // Here we are returning an object of type 'Vets' rather than a collection of Vet
+        // objects so it is simpler for JSon/Object mapping
         Vets vets = new Vets();
         vets.getVetList().addAll(this.clinicService.findVets());
         return vets;
     }
-
 
 }
